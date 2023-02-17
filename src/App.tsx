d4c79f8371b77021f7e9649a8e80327ccf79fdf8
huisu <huisu0429@naver.com>
2023-02-17 17:23:02 +0900

    CRA 설치

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Button variant='dark'>Dark</Button>
+    </div>
+  );
+}
+
+export default App;
