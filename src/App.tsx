e516f6fb9202771b992a7cb205d8b82c715069f2
huisu <huisu0429@naver.com>
2023-02-20 17:05:37 +0900

    배너 및 상품 이미지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
-import { Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <Button variant='dark'>Dark</Button>
+      <Navbar bg='light' variant='light'>
+        <Container>
+          <Navbar.Brand href='#home'>My Home Shop</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#features'>Cart</Nav.Link>
+            <Nav.Link href='#pricing'>My Page</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'></div>
+      <Container>
+        <Row>
+          <Col sm>
+            <img
+              src='https://codingapple1.github.io/shop/shoes1.jpg'
+              width='80%'
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+          <Col sm>
+            <img
+              src='https://codingapple1.github.io/shop/shoes2.jpg'
+              width='80%'
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+          <Col sm>
+            <img
+              src='https://codingapple1.github.io/shop/shoes3.jpg'
+              width='80%'
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
